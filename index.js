7773d2724f04e2aa8a86c314207e15f4c6076a79
simo <wambuisimon712@gmail.com>
2023-12-17 20:41:51 +0300

    first commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
+const cartRoute = require("./routes/cart");
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -16,6 +17,7 @@ mongoose
 //
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/carts", cartRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend Server is running");
